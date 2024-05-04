2fa51efc4501aae1fe42b099c8d063af9f19ce34
Daniel Rojas Grass <drgrassnk445@gmail.com>
2024-05-03 21:38:19 -0400

    axios

--- client/src/components/Presentation/index.tsx
+++ client/src/components/Presentation/index.tsx
@@ -13,7 +13,7 @@ const Presentation = () => {
                     <div className="-mx-4 flex flex-wrap">
                         <div className="w-full px-4">
                             <div className="mx-auto max-w-[800px] text-center">
-                                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                                <h1 className="mb-5 text-3xl font-bold leading-tight text-slate-900 dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                                     Construcciones menores
                                 </h1>
                                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
